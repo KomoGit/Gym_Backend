9e6f6ce31ae6acd38479dab0581a87c624c82067
Elnur <entreur@gmail.com>
2023-11-07 17:27:52 +0400

    Models|

---
+++ Model/Enum/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Model.Enum
+{
+    public enum Role
+    {
+        Administrator = 1,//Has access to delete,modify and create things such as members (even mods), blogs, comments, items etc.
+        Moderator = 2,
+        Member = 3,
+        Visitor = 4
+    }
+}
