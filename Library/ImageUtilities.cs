941cf5ae9e03f4ce8c83c16e9ed47921e77d9298
Elnur <entreur@gmail.com>
2023-11-14 11:26:18 +0400

    ++

---
+++ Library/ImageUtilities.cs
@@ -0,0 +1,14 @@
+﻿using API.Library.Repository;
+using ImageMagick;
+
+namespace API.Library
+{
+    public class ImageUtilities : IImageUtility
+    {
+        public void CompressImage(FileInfo rawImage)
+        {
+            ImageOptimizer optimizer = new();
+            optimizer.LosslessCompress(rawImage);
+        }
+    }
+}
